eb0a2acf56e51932cf9e1039d92805ae9768029b
Giada Alduenda <genaroalduenda@Giadas-MBP.attlocal.net>
2022-07-25 00:58:04 -0400

    finishing touches on assessment

---
+++ server/controller.js
@@ -0,0 +1,298 @@
+require('dotenv').config()
+
+const { CONNECTION_STRING } = process.env;
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
+        }
+    }
+});
+
+module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+            drop table if exists cities;
+            drop table if exists countries;
+
+            create table countries (
+                country_id serial primary key, 
+                name varchar
+            );
+
+            create table cities (
+                city_id serial primary key,
+                name varchar,
+                rating integer,
+                country_id integer
+
+            );
+            
+            insert into cities (name , rating, country_id)
+            values('Nashville', 9, 187),
+            ('Barcelona', 8, 165),
+            ('Puerto Vallarta', 10, 111);
+
+
+            insert into countries (name)
+            values ('Afghanistan'),
+            ('Albania'),
+            ('Algeria'),
+            ('Andorra'),
+            ('Angola'),
+            ('Antigua and Barbuda'),
+            ('Argentina'),
+            ('Armenia'),
+            ('Australia'),
+            ('Austria'),
+            ('Azerbaijan'),
+            ('Bahamas'),
+            ('Bahrain'),
+            ('Bangladesh'),
+            ('Barbados'),
+            ('Belarus'),
+            ('Belgium'),
+            ('Belize'),
+            ('Benin'),
+            ('Bhutan'),
+            ('Bolivia'),
+            ('Bosnia and Herzegovina'),
+            ('Botswana'),
+            ('Brazil'),
+            ('Brunei'),
+            ('Bulgaria'),
+            ('Burkina Faso'),
+            ('Burundi'),
+            ('Côte d''Ivoire'),
+            ('Cabo Verde'),
+            ('Cambodia'),
+            ('Cameroon'),
+            ('Canada'),
+            ('Central African Republic'),
+            ('Chad'),
+            ('Chile'),
+            ('China'),
+            ('Colombia'),
+            ('Comoros'),
+            ('Congo'),
+            ('Costa Rica'),
+            ('Croatia'),
+            ('Cuba'),
+            ('Cyprus'),
+            ('Czech Republic'),
+            ('Democratic Republic of the Congo'),
+            ('Denmark'),
+            ('Djibouti'),
+            ('Dominica'),
+            ('Dominican Republic'),
+            ('Ecuador'),
+            ('Egypt'),
+            ('El Salvador'),
+            ('Equatorial Guinea'),
+            ('Eritrea'),
+            ('Estonia'),
+            ('Eswatini'),
+            ('Ethiopia'),
+            ('Fiji'),
+            ('Finland'),
+            ('France'),
+            ('Gabon'),
+            ('Gambia'),
+            ('Georgia'),
+            ('Germany'),
+            ('Ghana'),
+            ('Greece'),
+            ('Grenada'),
+            ('Guatemala'),
+            ('Guinea'),
+            ('Guinea-Bissau'),
+            ('Guyana'),
+            ('Haiti'),
+            ('Holy See'),
+            ('Honduras'),
+            ('Hungary'),
+            ('Iceland'),
+            ('India'),
+            ('Indonesia'),
+            ('Iran'),
+            ('Iraq'),
+            ('Ireland'),
+            ('Israel'),
+            ('Italy'),
+            ('Jamaica'),
+            ('Japan'),
+            ('Jordan'),
+            ('Kazakhstan'),
+            ('Kenya'),
+            ('Kiribati'),
+            ('Kuwait'),
+            ('Kyrgyzstan'),
+            ('Laos'),
+            ('Latvia'),
+            ('Lebanon'),
+            ('Lesotho'),
+            ('Liberia'),
+            ('Libya'),
+            ('Liechtenstein'),
+            ('Lithuania'),
+            ('Luxembourg'),
+            ('Madagascar'),
+            ('Malawi'),
+            ('Malaysia'),
+            ('Maldives'),
+            ('Mali'),
+            ('Malta'),
+            ('Marshall Islands'),
+            ('Mauritania'),
+            ('Mauritius'),
+            ('Mexico'),
+            ('Micronesia'),
+            ('Moldova'),
+            ('Monaco'),
+            ('Mongolia'),
+            ('Montenegro'),
+            ('Morocco'),
+            ('Mozambique'),
+            ('Myanmar'),
+            ('Namibia'),
+            ('Nauru'),
+            ('Nepal'),
+            ('Netherlands'),
+            ('New Zealand'),
+            ('Nicaragua'),
+            ('Niger'),
+            ('Nigeria'),
+            ('North Korea'),
+            ('North Macedonia'),
+            ('Norway'),
+            ('Oman'),
+            ('Pakistan'),
+            ('Palau'),
+            ('Palestine State'),
+            ('Panama'),
+            ('Papua New Guinea'),
+            ('Paraguay'),
+            ('Peru'),
+            ('Philippines'),
+            ('Poland'),
+            ('Portugal'),
+            ('Qatar'),
+            ('Romania'),
+            ('Russia'),
+            ('Rwanda'),
+            ('Saint Kitts and Nevis'),
+            ('Saint Lucia'),
+            ('Saint Vincent and the Grenadines'),
+            ('Samoa'),
+            ('San Marino'),
+            ('Sao Tome and Principe'),
+            ('Saudi Arabia'),
+            ('Senegal'),
+            ('Serbia'),
+            ('Seychelles'),
+            ('Sierra Leone'),
+            ('Singapore'),
+            ('Slovakia'),
+            ('Slovenia'),
+            ('Solomon Islands'),
+            ('Somalia'),
+            ('South Africa'),
+            ('South Korea'),
+            ('South Sudan'),
+            ('Spain'),
+            ('Sri Lanka'),
+            ('Sudan'),
+            ('Suriname'),
+            ('Sweden'),
+            ('Switzerland'),
+            ('Syria'),
+            ('Tajikistan'),
+            ('Tanzania'),
+            ('Thailand'),
+            ('Timor-Leste'),
+            ('Togo'),
+            ('Tonga'),
+            ('Trinidad and Tobago'),
+            ('Tunisia'),
+            ('Turkey'),
+            ('Turkmenistan'),
+            ('Tuvalu'),
+            ('Uganda'),
+            ('Ukraine'),
+            ('United Arab Emirates'),
+            ('United Kingdom'),
+            ('United States of America'),
+            ('Uruguay'),
+            ('Uzbekistan'),
+            ('Vanuatu'),
+            ('Venezuela'),
+            ('Vietnam'),
+            ('Yemen'),
+            ('Zambia'),
+            ('Zimbabwe');
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => {
+            console.log('error seeding DB', err)
+            res.sendStatus(500)
+        })
+    },
+
+    getCountries: (req, res) => {
+        sequelize.query(`
+       select * from countries
+        `).then(dbRes => res.status(200).send(dbRes[0])).catch(err => {
+            console.log('error', err)
+            res.sendStatus(500)
+        })
+    },
+
+
+
+
+    createCity: (req, res) => {
+        const { name, rating, country_id } = req.body;
+        sequelize.query(`
+        insert into cities (name, rating, country_id) 
+        values ('${name}', ${rating}, ${country_id})
+         
+        `).then(dbRes => res.status(200).send(dbRes[0])).catch(err => {
+            console.log('error', err)
+            res.sendStatus(500)
+        })
+    },
+
+
+
+
+
+
+    getCities: (req, res) => {
+        sequelize.query(`
+        SELECT city_id, cities.name as city, rating, cities.country_id, countries.name as country
+        FROM cities
+        inner join countries on countries.country_id = cities.country_id
+        order by rating desc
+            `).then(dbRes => res.status(200).send(dbRes[0])).catch(err => {
+            console.log('error', err)
+            res.sendStatus(500)
+        })
+
+
+    },
+
+    deleteCity: (req, res) => {
+        const { id } = req.params;
+        sequelize.query(`
+        delete from cities
+        where ${id} = city_id
+        `).then(dbRes => res.status(200).send(dbRes[0])).catch(err => {
+            console.log('error', err)
+            res.sendStatus(500)
+        })
+    }
+}
